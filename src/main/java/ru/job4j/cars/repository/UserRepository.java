f7e5f2b7183338921e777a73fe07f68ec0e106fb
Paranoj <e.behterev@yandex.ru>
2023-03-11 14:46:54 +0700

    6. Лямбды и шаблон Command [#49295]

---
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -0,0 +1,83 @@
+package ru.job4j.cars.repository;
+
+import lombok.AllArgsConstructor;
+import ru.job4j.cars.model.User;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * @author: Egor Bekhterev
+ * @date: 08.03.2023
+ * @project: job4j_cars
+ */
+@AllArgsConstructor
+public class UserRepository {
+    private final CrudRepository crudRepository;
+
+    /**
+     * Сохранить в базе.
+     * @param user пользователь.
+     * @return пользователь с id.
+     */
+    public User create(User user) {
+        crudRepository.run(session -> session.persist(user));
+        return user;
+    }
+
+    /**
+     * Обновить в базе пользователя.
+     * @param user пользователь.
+     */
+    public void update(User user) {
+        crudRepository.run(session -> session.merge(user));
+    }
+
+    /**
+     * Удалить пользователя по id.
+     * @param userId ID
+     */
+    public void delete(int userId) {
+        crudRepository.run("DELETE FROM User WHERE id = :fId", Map.of("fId", userId));
+    }
+
+    /**
+     * Список пользователь отсортированных по id.
+     * @return список пользователей.
+     */
+    public List<User> findAllOrderById() {
+        return crudRepository.query("FROM User ORDER BY id ASC", User.class);
+    }
+
+    /**
+     * Найти пользователя по ID
+     * @return пользователь.
+     */
+    public Optional<User> findById(int id) {
+        return crudRepository.optional(
+                "FROM User WHERE id = :fId", User.class, Map.of("fId", id)
+        );
+    }
+
+    /**
+     * Список пользователей по login LIKE %key%
+     * @param key key
+     * @return список пользователей.
+     */
+    public List<User> findByLikeLogin(String key) {
+        return crudRepository.query(
+                "FROM User WHERE login like :fKey", User.class, Map.of("fKey", "%" + key + "%")
+        );
+    }
+
+    /**
+     * Найти пользователя по login.
+     * @param login login.
+     * @return Optional or user.
+     */
+    public Optional<User> findByLogin(String login) {
+        return crudRepository.optional(
+                "FROM User WHERE LOGIN = :fLogin", User.class, Map.of("fLogin", login));
+    }
+}
--- src/main/java/ru/job4j/cars/repository/UserUsage.java
+++ src/main/java/ru/job4j/cars/repository/UserUsage.java
@@ -1,4 +1,4 @@
-package ru.job4j.cars.model.repository;
+package ru.job4j.cars.repository;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
@@ -17,7 +17,7 @@ public class UserUsage {
                 .configure().build();
         try (SessionFactory sf = new MetadataSources(registry)
                 .buildMetadata().buildSessionFactory()) {
-            var userRepository = new UserRepository(sf);
+            var userRepository = new UserRepository(new CrudRepository(sf));
             var user = new User();
             user.setLogin("admin");
             user.setPassword("admin");
